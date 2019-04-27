bfcaa8cd68962c4a69c33579bcf447a79eb5fd0e
ksatola <krzysztof@satola.net>
2019-04-27 21:54:54 +0000

    Added heatmap

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Heatmap
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,6 +42,9 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    category_map = df.iloc[:, 4:].corr().values
+    category_names = list(df.iloc[:, 4:].columns)
+    
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -63,6 +66,19 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Heatmap(
+                    x=category_names,
+                    y=category_names[::-1],
+                    z=category_map
+                )
+            ],
+            
+            'layout': {
+                'title': 'Categories Heatmap'
+                }
         }
     ]
     
